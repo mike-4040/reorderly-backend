a86e1b4a554b310e1b32757f0a9966c1de528fc4
Mike Kravtsov <kravtsovmike@gmail.com>
2025-11-01 19:56:20 -0700

    linting and formatting (#3)

---
+++ functions/eslint.config.mjs
@@ -0,0 +1,49 @@
+// @ts-check
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig(
+  {
+    ignores: ['lib/**/*', 'eslint.config.mjs', 'package-lock.json'],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Node.js built-in modules
+            'external', // npm packages
+            'internal', // Internal modules
+            'parent', // Parent directories
+            'sibling', // Same directory
+            'index', // Index files
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+);
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
-
-import { onRequest } from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
+import { onRequest } from 'firebase-functions/https';
+import * as logger from 'firebase-functions/logger';
 
 export const helloWorld = onRequest((_request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
 });
