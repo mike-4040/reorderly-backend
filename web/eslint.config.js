fb9cf58f702df5befdab2c2cfe535a1209af599e
Mike Kravtsov <mike.kravtsov@homebot.ai>
2025-11-08 00:42:47 -0800

    feat: add @tanstack/eslint-plugin-router to ESLint configuration and package dependencies

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,18 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import pluginRouter from "@tanstack/eslint-plugin-router";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist', 'src/routeTree.gen.ts']),
+  globalIgnores(["dist", "src/routeTree.gen.ts"]),
+  ...pluginRouter.configs["flat/recommended"],
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
@@ -20,4 +22,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
